9599774e26d09887f87a4cde872f2e002bf424bb
David Luna de León <davidlunadeleon@gmail.com>
2022-06-10 15:39:50 -0500

    Update setup.py to find inner packages of movies

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="movies",
     version="0.1",
-    packages=["movies"],
+    packages=find_packages(where=".", include="movies*"),
 )
