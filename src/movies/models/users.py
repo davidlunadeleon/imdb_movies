69b37759c9760f8577b18fdd6459a7d50d4eea38
David Luna de León <davidlunadeleon@gmail.com>
2022-06-11 22:38:31 -0500

    Add stepts to hash the password in User
    - Add step in __post_init__ to hash the passed password on new users.

--- src/movies/models/users.py
+++ src/movies/models/users.py
@@ -1,14 +1,17 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, InitVar
 from datetime import datetime
+from movies.util.password import hash
 
 
 @dataclass(unsafe_hash=True)
 class User:
-    password_hash: str
-    preference_key: int
     username: str
-    create_time: datetime | None = None
+    preference_key: int
+    password: InitVar[str]
     user_id: int | None = None
+    create_time: datetime | None = None
+    password_hash: bytes | None = None
 
-    def __post_init__(self):
+    def __post_init__(self, password: str):
         self.create_time = datetime.now()
+        self.password_hash = hash(password)
