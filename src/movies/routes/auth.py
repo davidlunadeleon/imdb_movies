839ee2fced22ff50ad34cc05a7ef4d62964e2a0d
David Luna de León <davidlunadeleon@gmail.com>
2022-06-11 22:00:02 -0500

    Update auth routes to use global context
    - Retrieve the repository from the global context user_repository

--- src/movies/routes/auth.py
+++ src/movies/routes/auth.py
@@ -1,13 +1,14 @@
-from datetime import datetime
-from flask import Blueprint, request, current_app as app
+from flask import Blueprint, request, g
 from flask.wrappers import Response
 from movies.models.users import User
 from movies.repository import UserRepository
 from movies.util.enums import MovieGenres
-from random import randint
 import numpy as np
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+user_repository: UserRepository
+user_repository = g.user_repository
 
 
 def get_preference_key(preferences) -> int:
@@ -38,15 +39,10 @@ def signup():
     preference1 = request.form["preference1"]
     preference2 = request.form["preference2"]
     preference3 = request.form["preference3"]
-    create_time = datetime.now()
     preference_key = get_preference_key(
         [get_genre(preference1), get_genre(preference2), get_genre(preference3)]
     )
 
     user = User(password, preference_key, username)
-    repo = UserRepository(app.session)
-    repo.add(user)
-    app.session.commit()
-    l = repo.list()
-    print(l)
+    user_repository.add(user)
     return Response(response="ok", status=200)
