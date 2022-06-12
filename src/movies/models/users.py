09f843a354504f7864bba92f013f2f52a6b902af
David Luna de León <davidlunadeleon@gmail.com>
2022-06-12 00:46:19 -0500

    Update user model with function check_credentials

--- src/movies/models/users.py
+++ src/movies/models/users.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, InitVar
 from datetime import datetime
-from movies.util.password import hash
+from movies.util.password import hash, compare
 
 
 @dataclass(unsafe_hash=True)
@@ -15,3 +15,10 @@ class User:
     def __post_init__(self, password: str):
         self.create_time = datetime.now()
         self.password_hash = hash(password)
+
+    def check_credentials(self, username: str, password: str) -> bool:
+        return (
+            self.username == username
+            and self.password_hash is not None
+            and compare(self.password_hash, password)
+        )
