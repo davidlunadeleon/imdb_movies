5edf0975d8532b01caef8f6b9a59a27059d34737
David Luna de León <davidlunadeleon@gmail.com>
2022-06-11 22:59:14 -0500

    Remove shared files, since these are now in movies

--- src/shared/constants.py
+++
@@ -1,7 +0,0 @@
-movies = {
-  "comedy": 1,
-  "drama": 2,
-  "sci-fi": 3,
-  "romantic": 4,
-  "adventure": 5
-}
--- src/shared/operations.py
+++
@@ -1,5 +0,0 @@
-import numpy
-
-def get_preference_key(preferences):
-  product = numpy.prod(preferences)
-  return (product % 5) + 1
