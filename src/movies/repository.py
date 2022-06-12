3e3beec417ddc37a1412f6eb74a64b41a1eaa99c
David Luna de León <davidlunadeleon@gmail.com>
2022-06-11 21:13:51 -0500

    Define repositories for Movies and Users

---
+++ src/movies/repository.py
@@ -0,0 +1,54 @@
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar, TypeVar, TypeVar, TypeVar
+from sqlalchemy.orm import Session
+from movies.models.users import User
+from movies.models.movies import Movie
+
+T = TypeVar("T", User, Movie)
+
+
+class AbstractRepository(ABC, Generic[T]):
+    session: Session
+
+    def __init__(self, session: Session):
+        self.session = session
+
+    def add(self, entity: T):
+        self.session.add(entity)
+
+    def update(self, entity: T) -> T:
+        return self.session.merge(entity)
+
+    @abstractmethod
+    def delete(self, id: int):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_by_id(self, id: int) -> T:
+        raise NotImplementedError
+
+    @abstractmethod
+    def list(self) -> list[T]:
+        raise NotImplementedError
+
+
+class UserRepository(AbstractRepository[User]):
+    def delete(self, id: int):
+        return self.session.query(User).filter_by(user_id=id).delete()
+
+    def list(self):
+        return self.session.query(User).all()
+
+    def get_by_id(self, id: int) -> User:
+        return self.session.query(User).filter_by(user_id=id).one()
+
+
+class MovieRepository(AbstractRepository[Movie]):
+    def delete(self, id: int):
+        return self.session.query(Movie).filter_by(movie_id=id).delete()
+
+    def list(self):
+        return self.session.query(Movie).all()
+
+    def get_by_id(self, id: int) -> Movie:
+        return self.session.query(Movie).filter_by(movie_id=id).one()
